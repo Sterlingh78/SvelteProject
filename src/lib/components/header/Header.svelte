bd605d17ccf6dbbdc99df7f4319cc3359be19641
sterlingh78 <sterlingh78@yahoo.com>
2022-10-16 17:52:18 -0600

    added dropdown menu and styled signup

--- src/lib/components/header/Header.svelte
+++ src/lib/components/header/Header.svelte
@@ -1,3 +1,6 @@
+<script>
+    let themeOptions = ['light', 'dark', 'cupcake', 'aqua', 'dracula', 'winter']
+</script>
 <div class="navbar bg-base-100">
     <div class="flex-1">
         <a class="btn btn-ghost normal-case text-xl" href="/"><img src="/home.svg" width="30em" alt="home icon"/></a>
@@ -6,4 +9,31 @@
         <a class="btn btn-ghost normal-case text-xl" href="/players">NBA Players</a>
         <a class="btn btn-ghost normal-case text-xl" href="/dashboard">Dashboard</a>
     </div>
+    <div class="dropdown dropdown-end">
+		<label tabindex="0" class="btn btn-ghost btn-circle avatar m-1" for="userIcon">
+			<div class="w-16 rounded-full">
+				<img id="userIcon" src="https://placeimg.com/90/90/people" alt="User icon" />
+			</div>
+		</label>
+		<ul
+			tabindex="0"
+			class="mt-3 p-2 dropdown-content bg-base-100 menu menu-compact shadow w-36"
+		>
+			<li><a>Profile</a></li>
+			<li><a>Settings</a></li>
+            <li><a href="/auth/signup">Sign Up</a></li>
+            <li><a href="/auth/login">Log In</a></li>
+			<li>
+				<select class="select w-full max-x-xs">
+					<option>Theme</option>
+					{#each themeOptions as theme}
+						<option value={theme}>
+							{theme}
+						</option>
+					{/each}
+				</select>
+			</li>
+			<li><a>Logout</a></li>
+		</ul>
+	</div>
 </div>
--- src/routes/auth/signup/+page.svelte
+++ src/routes/auth/signup/+page.svelte
@@ -15,44 +15,44 @@
 
 <div class="hero min-h-screen bg-base-200">
 	<div class="hero-content flex-col lg:flex-row-reverse">
-	  <div class="text-center lg:text-left">
+	  <div class="text-center lg:text-left m-4">
 		<h1 class="text-5xl font-bold">Sign Up</h1>
 		<p class="py-6 mt-4">Enter basic information to access this Svelte site. No real information required.</p>
 	  </div>
-	  <div class="flex-shrink-0 w-full max-w-sm shadow-2xl">
+	  <div class="flex-shrink-0 w-full max-w-sm">
 		<form method="POST" action="?/signup" use:enhance>
 		  <div class="form-control">
 			<label class="label">
 			  <span class="label-text">First Name</span>
 			</label>
-			<input type="text" name="fname" required placeholder="first name" class="input input-bordered" />
+			<input type="text" name="fname" required placeholder="first name" class="input input-accent input-bordered" />
 		  </div>
 		  <div class="form-control">
 			<label class="label">
 			  <span class="label-text">Last Name</span>
 			</label>
-			<input type="text" name="lname" required placeholder="last name" class="input input-bordered" />
+			<input type="text" name="lname" required placeholder="last name" class="input input-accent input-bordered" />
 		  </div>
 		  <div class="form-control">
 			<label class="label">
 			  <span class="label-text">Email</span>
 			</label>
-			<input type="text" name="email" required placeholder="email" class="input input-bordered" />
+			<input type="text" name="email" required placeholder="email" class="input input-accent input-bordered" />
 		  </div>
 		  <div class="form-control">
 			<label class="label">
 			  <span class="label-text">Password</span>
 			</label>
-			<input type="password" name="password" required placeholder="password" class="input input-bordered" bind:value={passwords.first} />
+			<input type="password" name="password" required placeholder="password" class="input input-accent input-bordered" bind:value={passwords.first} />
 		  </div>
 		  <div class="form-control">
 			<label class="label">
 			  <span class="label-text">Confirm Password</span>
 			</label>
-			<input type="password" name="confirmPassword" required placeholder="confirm password" class="input input-bordered" bind:value={passwords.confirm} />
+			<input type="password" name="confirmPassword" required placeholder="confirm password" class="input input-accent input-bordered" bind:value={passwords.confirm} />
 		  </div>
 		  <div class="form-control mt-4">
-			<button class="btn btn-primary" type="submit" disabled bind:this={button}>Login</button>
+			<button class="btn btn-primary" type="submit" disabled bind:this={button}>Submit</button>
 		  </div>
 		</form>
 	  </div>
