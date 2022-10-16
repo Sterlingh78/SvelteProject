49c7128f804975a094d6d95f98a168ff4c4d3902
sterlingh78 <sterlingh78@yahoo.com>
2022-10-15 20:38:24 -0600

    added click event for article detail on news route

--- src/routes/news/+page.svelte
+++ src/routes/news/+page.svelte
@@ -1,6 +1,27 @@
 <script>
     export let data;
     let articles = data.newsData.articles;
+    $: pageContent = {
+      img: '',
+      title: '',
+      body: '',
+      url: ''
+    }
+
+    function populateContent(event) {
+      let id = event.target.id
+
+      for (let i = 0; i < articles.length; i++) {
+        if (id == articles[i].url) {
+          pageContent.img = articles[i].urlToImage
+          pageContent.title = articles[i].title
+          let rawContent = articles[i].content
+          let cleanedContent = rawContent.slice(0, 150)
+          pageContent.body = cleanedContent
+          pageContent.url = articles[i].url
+        }
+      }
+    }
 </script>
 
 <main>
@@ -8,7 +29,25 @@
     <input id="my-drawer" type="checkbox" class="drawer-toggle" />
     <div class="drawer-content flex flex-col items-center justify-center">
       <!-- Page content here -->
-      <label for="my-drawer" class="btn btn-primary drawer-button">View Articles</label>
+      {#if pageContent.img == ''}
+      <p></p>
+      {:else}
+      <div class="card w-96 bg-base-100 shadow-xl">
+        <figure class="px-10 pt-10">
+          <img src={pageContent.img} alt="Shoes" class="rounded-xl" />
+        </figure>
+        <div class="card-body items-center text-center">
+          <h2 class="card-title">{pageContent.title}</h2>
+          <p>{pageContent.body}</p>
+          <div class="card-actions">
+            <a href={pageContent.url} target="_blank" rel="noreferrer">
+              <button class="btn btn-primary">Source</button>
+            </a>
+          </div>
+        </div>
+      </div>
+      {/if}
+      <label for="my-drawer" class="btn btn-primary drawer-button mt-4">View Articles</label>
       <div class="hero bg-base-200">
       </div>
     </div> 
@@ -17,11 +56,11 @@
       <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
         <!-- Sidebar content here -->
         {#each articles as article}
-        <li><a href={article.url} target="_blank">
+        <li><a href="" on:click={populateContent}>
           <div class="card shadow-xl image-full">
             <figure><img src={article.urlToImage} alt="Shoes" /></figure>
             <div class="card-body">
-              <h2 class="card-title">{article.title}</h2>
+              <h2 class="card-title" id={article.url}>{article.title}</h2>
             </div>
           </div>
         </a></li>
